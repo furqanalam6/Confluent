007d63d8e626be843d2a6433ab716231cee3e9b7
unknown <furqanalam6@inseyab.com>
2022-07-23 19:21:52 +0500

    new

--- stream_job.py
+++ stream_job.py
@@ -150,10 +150,10 @@ joining_result = hca.join(ooh, hca["CUST_ACCOUNT_ID"] == ooh["SOLD_TO_ORG_ID"]) 
     #             .join(haou, ooh["SHIP_FROM_ORG_ID"] == haou["ORGANIZATION_ID"]) \
                     # .join(hp, hca["party_id"] == hp["party_id"])
 
-# query = joining_result \
-#     .writeStream \
-#     .format("console") \
-#     .start().awaitTermination()
+query = joining_result \
+    .writeStream \
+    .format("console") \
+    .start().awaitTermination()
 
 # database = "STCC"
 # table = "dbo.complex_query"
