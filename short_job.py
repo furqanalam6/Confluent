005ca93fa8b4389796e3faf28323faeae3da0f90
unknown <furqanalam6@inseyab.com>
2022-07-23 15:43:54 +0500

    new

--- short_job.py
+++ short_job.py
@@ -56,6 +56,9 @@ database = "TestDB"
 table = "dbo.PARTY"
 user = "SA"
 password  = "MhffPOC2022"
+db_target_url = "jdbc:sqlserver://10.92.26.184:1433;TestDB"
+db_target_properties = {"user":"SA", "password":"MhffPOC2022"}
+
  
 #read table data into a spark dataframe
 jdbcDF = spark.read.format("jdbc") \
@@ -69,24 +72,43 @@ jdbcDF = spark.read.format("jdbc") \
 #show the data loaded into dataframe
 jdbcDF.show()
 
-# db_target_url = "jdbc:sqlserver://10.92.26.184:1433;TestDB"
-# db_target_properties = {"user":"SA", "password":"MhffPOC2022"}
 
-# def writesql(dff, epoch_id):
-#     dfwrite = dff.write.mode("overwrite") \
-#     .format("jdbc") \
-#     .option("url", f"jdbc:sqlserver://10.92.26.184:1433;databaseName={database};") \
-#     .option("dbtable", table) \
-#     .option("user", user) \
-#     .option("password", password) \
-#     .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
-#     .save()
+def writesql(dff, epoch_id):
+    dff.write.mode("overwrite") \
+        .format("jdbc") \
+        .option("url", f"jdbc:sqlserver://localhost:1433;databaseName={database};") \
+        .option("dbtable", table) \
+        .option("user", user) \
+        .option("password", password) \
+        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+        .save()
 
-#     dfwrite.jdbc(url=db_target_url, table="PARTY", properties=db_target_properties) # if this is not working use below
-#     #df.write.jdbc(url=jdbcurl, table=table_name, properties=db_properties, mode="append")
-#     pass
+    # dff.jdbc(url=db_target_url, table="PARTY", properties=db_target_properties) # if this is not working use below
+    # #df.write.jdbc(url=jdbcurl, table=table_name, properties=db_properties, mode="append")
+    # pass
 
 
-# query = joining_result.writeStream.outputMode("append").foreachBatch(writesql).start()
-# query.awaitTermination()
+query = hp.writeStream.outputMode("append").foreachBatch(writesql).start()
+query.awaitTermination()
+
+#read table data into a spark dataframe
+jdbcDF = spark.read.format("jdbc") \
+    .option("url", f"jdbc:sqlserver://10.92.26.184:1433;databaseName={database};") \
+    .option("dbtable", table) \
+    .option("user", user) \
+    .option("password", password) \
+    .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+    .load()
+ 
+#show the data loaded into dataframe
+jdbcDF.show()
+
+# #set variable to be used to connect the database
+# database = "TestDB"
+# table = "dbo.tbl_spark_df"
+# user = "test"
+# password  = "*****"
+ 
+#write the dataframe into a sql table
+
 
