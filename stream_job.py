b23801447af752682fda5509f55186566acbd418
unknown <furqanalam6@inseyab.com>
2022-07-24 12:31:34 +0500

    new

--- stream_job.py
+++ stream_job.py
@@ -152,7 +152,7 @@ ool = OE_ORDER_LINES_ALL.selectExpr("substring(value, 6) as value") \
     #             .join(haou, ooh["SHIP_FROM_ORG_ID"] == haou["ORGANIZATION_ID"]) \
                     # .join(hp, hca["party_id"] == hp["party_id"])
 
-query = ooh \
+query = ool \
     .writeStream \
     .format("console") \
     .start().awaitTermination()
