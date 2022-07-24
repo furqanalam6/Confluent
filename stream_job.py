870e4fbcc1127815eb63cb3986c6a63429cbddf7
unknown <furqanalam6@inseyab.com>
2022-07-24 21:43:53 +0500

    new

--- stream_job.py
+++ stream_job.py
@@ -145,7 +145,7 @@ ool = OE_ORDER_LINES_ALL.selectExpr("substring(value, 6) as value") \
 #     .join(ot, ooh["ORDER_TYPE_ID"] == ot["TRANSACTION_TYPE_ID"]) \
 #         .join(ottt, ot["TRANSACTION_TYPE_ID"] == ottt["TRANSACTION_TYPE_ID"])
 # 
-joining_result =  ot.join(ottt, "TRANSACTION_TYPE_ID") 
+# joining_result =  ot.join(ottt, "TRANSACTION_TYPE_ID") 
 
 # hca.join(ooh, func.round(hca["CUST_ACCOUNT_ID"]) == func.round(ooh["SOLD_TO_ORG_ID"])) \
 
@@ -161,7 +161,7 @@ joining_result =  ot.join(ottt, "TRANSACTION_TYPE_ID")
 #                 .join(haou, ooh["SHIP_FROM_ORG_ID"] == haou["ORGANIZATION_ID"]) \
 #                     .join(hp, hca["party_id"] == hp["party_id"])
 
-query = joining_result \
+query = ooh \
     .writeStream \
     .format("console") \
     .start().awaitTermination()
