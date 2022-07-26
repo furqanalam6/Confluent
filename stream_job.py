a778c0f48b645b46571065f53353e75b70d436e9
unknown <furqanalam6@inseyab.com>
2022-07-26 15:59:21 +0500

    8 schema + code

--- stream_job.py
+++ stream_job.py
@@ -197,23 +197,23 @@ joining_result = hp.join(hca, "PARTY_ID") \
 #     .format("console") \
 #     .start().awaitTermination()
 
-database = "STCC"
-table = "dbo.device_sales_table"
-user = "SA"
-password  = "MhffPOC2022"
+# database = "STCC"
+# table = "dbo.device_sales_table"
+# user = "SA"
+# password  = "MhffPOC2022"
 
-def writesql(dff, epoch_id):
-    dff.write.mode("overwrite") \
-        .format("jdbc") \
-        .option("url", f"jdbc:sqlserver://10.92.26.184:1433;databaseName={database};") \
-        .option("dbtable", table) \
-        .option("user", user) \
-        .option("password", password) \
-        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
-        .save()
+# def writesql(dff, epoch_id):
+#     dff.write.mode("overwrite") \
+#         .format("jdbc") \
+#         .option("url", f"jdbc:sqlserver://10.92.26.184:1433;databaseName={database};") \
+#         .option("dbtable", table) \
+#         .option("user", user) \
+#         .option("password", password) \
+#         .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+#         .save()
 
-query = joining_result.writeStream.outputMode("append").foreachBatch(writesql).start()
-query.awaitTermination()
+# query = joining_result.writeStream.outputMode("append").foreachBatch(writesql).start()
+# query.awaitTermination()
 
 
 
