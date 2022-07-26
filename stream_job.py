b241ed26c8c08da189bafc333c4eed1aea61ac75
unknown <furqanalam6@inseyab.com>
2022-07-26 16:51:35 +0500

    8 schema + code

--- stream_job.py
+++ stream_job.py
@@ -115,14 +115,14 @@ hca = HZ_CUST_ACCOUNTS.selectExpr("substring(value, 6) as value") \
 # Perfectly Working
 haou = HR_ALL_ORGANIZATION_UNITS.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_hr).alias("haou")) \
-        .select("haou.ORGANIZATION_ID") \
+        # .select("haou.ORGANIZATION_ID") 
             .filter("haou.BUSINESS_GROUP_ID = 101")
 
 # yet to test
 inv = MTL_SYSTEM_ITEMS_B.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_inv).alias("inv")) \
-        .select("inv.DESCRIPTION", "inv.SEGMENT1") \
-            .filter("inv.ORGANIZATION_ID = 105")
+        .select("inv.DESCRIPTION", "inv.SEGMENT1") 
+            # .filter("inv.ORGANIZATION_ID = 105")
 
 # Perfectly Working
 ot = OE_TRANSACTION_TYPES_ALL.selectExpr("substring(value, 6) as value") \
@@ -131,8 +131,8 @@ ot = OE_TRANSACTION_TYPES_ALL.selectExpr("substring(value, 6) as value") \
 # Perfectly Working
 ottt = OE_TRANSACTION_TYPES_TL.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_oe_tl).alias("ottt")) \
-        .select("ottt.TRANSACTION_TYPE_ID") \
-            .filter("ottt.LANGUAGE = 'US'")
+        .select("ottt.TRANSACTION_TYPE_ID") 
+            # .filter("ottt.LANGUAGE = 'US'")
 # .filter("ottt.TRANSACTION_TYPE_ID == 1226.0")
 # Perfectly Working
 ooh = OE_ORDER_HEADERS_ALL.selectExpr("substring(value, 6) as value") \
@@ -152,8 +152,8 @@ ool = OE_ORDER_LINES_ALL.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_oe_lines_all).alias("ool")) \
         .select( "ool.CREATION_DATE", "ool.LAST_UPDATE_DATE", "ool.LINE_CATEGORY_CODE" \
             ,  "ool.UNIT_LIST_PRICE", "ool.ORDERED_QUANTITY" \
-                , "ool.ORDERED_ITEM","ool.HEADER_ID") \
-                    .filter("ool.LAST_UPDATE_DATE = '2022-01-01'")
+                , "ool.ORDERED_ITEM","ool.HEADER_ID")
+                    # .filter("ool.LAST_UPDATE_DATE = '2022-01-01'")
 
                     # .filter("ool.FLOW_STATUS_CODE  = 'CLOSED'")
 # .filter("ool.LAST_UPDATE_DATE >= '2022-01-01'")
