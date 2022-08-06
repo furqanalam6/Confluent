73a866b284dbd49f94d0c7ce842ddc9882a81494
unknown <furqanalam6@inseyab.com>
2022-08-06 15:01:46 +0500

    elasticsearch connector

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -1,8 +1,8 @@
 from ntpath import join
 from pyspark.sql import SparkSession
 # , SaveMode, Row, DataFrame
-from pyspark.sql.avro.functions import from_avro
-from pyspark.sql.functions import col, expr, to_avro
+from pyspark.sql.avro.functions import from_avro, to_avro
+from pyspark.sql.functions import col, expr
 import pyspark.sql.functions as func
 # from pyspark.sql.functions import *
 # from pyspark.sql.types import *
