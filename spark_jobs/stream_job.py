fcb45c90bc020280526da0778775db1507be15ed
unknown <furqanalam6@inseyab.com>
2022-08-06 20:30:24 +0500

    elasticsearch connector

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -226,3 +226,11 @@ print("start to write")
 #       .start() \
 #       .awaitTermination()
 # [PARTY_ID: bigint, TRANSACTION_TYPE_ID: double, HEADER_ID: double, ORDER_TYPE_ID: double, SHIP_FROM_ORG_ID: double, SOLD_TO_ORG_ID: double, ORDERED_DATE: date, CREATION_DATE: date, LAST_UPDATE_DATE: date, LINE_CATEGORY_CODE: string, UNIT_LIST_PRICE: double, ORDERED_QUANTITY: double, ORDERED_ITEM: string, ATTRIBUTE4: string, ATTRIBUTE6: string, CUST_ACCOUNT_ID: bigint, ORGANIZATION_ID: bigint, DESCRIPTION: string, SEGMENT1: string]
+
+joining_result\
+    .writeStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
+    .option("topic", "kontext-kafka-3") \
+    .option("checkpointLocation", "checkpoint") \
+    .start().awaitTermination()
