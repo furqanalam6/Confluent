c602559aa8153e8efcb1b21d0c5ae5f1108d9431
unknown <furqanalam6@inseyab.com>
2022-08-07 20:45:49 +0500

    elasticsearch connector

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -204,7 +204,7 @@ print("join successfull")
 
 # print("start to write")
 query = hp \
-            .selectExpr("CAST(to_avro(struct(*)) as string) AS value") \
+            .selectExpr("to_avro(struct(*) AS value") \
             .writeStream \
             .format("kafka") \
             .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
