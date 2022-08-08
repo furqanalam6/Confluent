d13930ba891481e58700cace59d71f40bf69d0b0
unknown <furqanalam6@inseyab.com>
2022-08-08 20:06:41 +0500

    elasticsearch connector

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -125,14 +125,14 @@ hca = HZ_CUST_ACCOUNTS.selectExpr("substring(value, 6) as value") \
 # Perfectly Working
 haou = HR_ALL_ORGANIZATION_UNITS.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_hr).alias("haou")) \
-        .select("haou.ORGANIZATION_ID") 
-            # .filter("haou.BUSINESS_GROUP_ID = 101")
+        .select("haou.ORGANIZATION_ID") \
+            .filter("haou.BUSINESS_GROUP_ID = 101")
 
 # yet to test
 inv = MTL_SYSTEM_ITEMS_B.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_inv).alias("inv")) \
-        .select("inv.DESCRIPTION", "inv.SEGMENT1") 
-            # .filter("inv.ORGANIZATION_ID = 105")
+        .select("inv.DESCRIPTION", "inv.SEGMENT1") \
+            .filter("inv.ORGANIZATION_ID = 105")
 
 # Perfectly Working
 ot = OE_TRANSACTION_TYPES_ALL.selectExpr("substring(value, 6) as value") \
@@ -141,8 +141,8 @@ ot = OE_TRANSACTION_TYPES_ALL.selectExpr("substring(value, 6) as value") \
 # Perfectly Working
 ottt = OE_TRANSACTION_TYPES_TL.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_oe_tl).alias("ottt")) \
-        .select("ottt.TRANSACTION_TYPE_ID") 
-            # .filter("ottt.LANGUAGE = 'US'")
+        .select("ottt.TRANSACTION_TYPE_ID") \
+            .filter("ottt.LANGUAGE = 'US'")
 # Perfectly Working
 ooh = OE_ORDER_HEADERS_ALL.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_oe_headers_all).alias("ooh")) \
@@ -155,8 +155,8 @@ ool = OE_ORDER_LINES_ALL.selectExpr("substring(value, 6) as value") \
         .select( "ool.CREATION_DATE", "ool.LAST_UPDATE_DATE", "ool.LINE_CATEGORY_CODE" \
             ,  "ool.UNIT_LIST_PRICE", "ool.ORDERED_QUANTITY" \
                 , "ool.ORDERED_ITEM","ool.HEADER_ID") \
-                    .filter("ool.FLOW_STATUS_CODE  = 'CLOSED'") 
-                        # .filter("ool.LAST_UPDATE_DATE >= '2022-01-01'")
+                    .filter("ool.FLOW_STATUS_CODE  = 'CLOSED'") \
+                        .filter("ool.LAST_UPDATE_DATE >= '2022-01-01'")
 
 print("ready to join")
 # Join
