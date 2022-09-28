17404329736df5be02a48a247e748021908bfd88
unknown <furqanalam6@inseyab.com>
2022-09-28 12:28:22 +0500

    New Live Connection Data

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -21,65 +21,73 @@ spark.sparkContext.setLogLevel('ERROR')
 HZ_PARTIES = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.AR.HZ_PARTIES") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 HZ_CUST_ACCOUNTS = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.AR.HZ_CUST_ACCOUNTS") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 HR_ALL_ORGANIZATION_UNITS = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.HR.HR_ALL_ORGANIZATION_UNITS") \
+    .option("minPartitions",20) \
     .option("startingOffsets", "earliest") \
     .load()
 
 MTL_SYSTEM_ITEMS_B = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.INV.MTL_SYSTEM_ITEMS_B") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 OE_TRANSACTION_TYPES_ALL = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.ONT.OE_TRANSACTION_TYPES_ALL") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 OE_TRANSACTION_TYPES_TL = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.ONT.OE_TRANSACTION_TYPES_TL") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 OE_ORDER_HEADERS_ALL = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.ONT.OE_ORDER_HEADERS_ALL") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 OE_ORDER_LINES_ALL = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
+    .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
     .option("subscribe", "SPROD.ONT.OE_ORDER_LINES_ALL") \
     .option("startingOffsets", "earliest") \
+    .option("minPartitions",20) \
     .load()
 
 with open('/opt/Confluent/schemas/hz_parties.json','r') as f:
@@ -118,13 +126,13 @@ hca = HZ_CUST_ACCOUNTS.selectExpr("substring(value, 6) as value") \
 haou = HR_ALL_ORGANIZATION_UNITS.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_hr).alias("haou")) \
         .select("haou.ORGANIZATION_ID") \
-            .filter("haou.BUSINESS_GROUP_ID = 21")
+            .filter("haou.BUSINESS_GROUP_ID = 101")
 
 # yet to test
 inv = MTL_SYSTEM_ITEMS_B.selectExpr("substring(value, 6) as value") \
     .select(from_avro(col("value"), schema_inv).alias("inv")) \
         .select("inv.DESCRIPTION", "inv.SEGMENT1") \
-            .filter("inv.ORGANIZATION_ID = 25")
+            .filter("inv.ORGANIZATION_ID = 105")
 
 # Perfectly Working
 ot = OE_TRANSACTION_TYPES_ALL.selectExpr("substring(value, 6) as value") \
@@ -163,7 +171,7 @@ joining_result = ooh.join(ool, "HEADER_ID") \
 print("join successfull")
 # joining_result.printSchema()
 # print("ready to write on console")
-# query = ottt \
+# query = joining_result \
 #     .writeStream \
 #     .format("console") \
 #     .start().awaitTermination()
@@ -173,12 +181,12 @@ print("join successfull")
 # database = "STCC"
 # table = "dbo.device_sales_tables_new"
 # user = "SA"
-# password  = "MhffPOC222"
+# password  = "MhffPOC2022"
 # # intvl = 0
 # def writesql(dff, epoch_id):
 #     dff.write.mode("overwrite") \
 #         .format("jdbc") \
-#         .option("url", f"jdbc:sqlserver://2.92.26.184:1433;databaseName={database};") \
+#         .option("url", f"jdbc:sqlserver://10.92.26.184:1433;databaseName={database};") \
 #         .option("dbtable", table) \
 #         .option("user", user) \
 #         .option("password", password) \
@@ -192,8 +200,8 @@ query = joining_result \
             .selectExpr("to_json(struct(*)) AS value") \
             .writeStream \
             .format("kafka") \
-            .option("kafka.bootstrap.servers", "2.92.26.188:29093") \
-            .option("checkpointLocation", "checkpoint-location-11") \
+            .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
+            .option("checkpointLocation", "checkpoint-location-12") \
             .option("topic", "complex-query-result") \
             .start().awaitTermination() 
 
