a70b3971f9b5ce0e22716eb924d0b5827d2f4d8c
unknown <furqanalam6@inseyab.com>
2022-09-29 14:40:11 +0500

    New Live Connection Data

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -207,7 +207,7 @@ query = joining_result \
             .writeStream \
             .format("kafka") \
             .option("kafka.bootstrap.servers", "10.92.26.188:29093") \
-            .option("checkpointLocation", "checkpoint-location-12") \
-            .option("topic", "complex-query-result-0") \
+            .option("checkpointLocation", "checkpoint-location-0") \
+            .option("topic", "complex-query-result") \
             .start().awaitTermination() 
 
