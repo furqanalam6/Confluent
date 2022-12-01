9745d7ff87344a689c7e64d5893dd753370f2cdf
unknown <furqanalam6@inseyab.com>
2022-12-01 18:43:38 +0500

    check

--- spark_jobs/stream_job.py
+++ spark_jobs/stream_job.py
@@ -178,7 +178,7 @@ print("ready to join")
 # print("join successfull")
 # joining_result.printSchema()
 print("ready to write on console")
-query = ool \
+query = ooh \
     .writeStream \
     .format("console") \
     .start().awaitTermination()
